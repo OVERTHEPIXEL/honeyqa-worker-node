f8ada549c12a4a1c39006ae8737a12e086a037e9
devholic <devholic@plusquare.com>
2015-11-23 12:19:57 +0900

    Update atosl.js

--- atosl/atosl.js
+++ atosl/atosl.js
@@ -4,12 +4,32 @@ java.classpath.push("./src");
 var Atosl = java.newInstanceSync("io.honeyqa.atosl.Atosl");
 
 module.exports = {
-    getArch: function(path){
+    getArch: function(path) {
         var result = [];
         var rawArch = Atosl.getArchSync(path);
-        for (var i = 0; i < rawArch.sizeSync(); i++) {
-            result.push(rawArch.getSync(i));
+        if (rawArch != null) {
+            for (var i = 0; i < rawArch.sizeSync(); i++) {
+                result.push(rawArch.getSync(i));
+            }
+            return result;
+        } else {
+            return null;
         }
-        return result;
+    },
+    getUUID: function(arch, path) {
+        return Atosl.getUUIDSync(arch, path);
+    },
+    symbolicate: function(arch, path, addresses) {
+        var result = [];
+        var converted = java.newArray('java.lang.String', addresses);
+        var rawSymbolicated = Atosl.doSymbolicateSync(arch, path, converted);
+        if (rawSymbolicated != null) {
+            for (var i = 0; i < rawSymbolicated.sizeSync(); i++) {
+                result.push(rawSymbolicated.getSync(i));
+            }
+            return result;
+        } else {
+            return null;
+        }
     }
 }
